09422334777b2ab930063c6bc9b265a0a8b55e99
Amber(LP) <longamber613@gmail.com>
2024-01-25 17:52:58 -0500

    Add project files.

---
+++ FTP_Server/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace FTP_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDest = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblConnection = new System.Windows.Forms.Label();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPhoto = new System.Windows.Forms.TextBox();
+            this.btnPhoto = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(279, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Target Destination";
+            // 
+            // lblDest
+            // 
+            this.lblDest.AutoSize = true;
+            this.lblDest.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDest.Location = new System.Drawing.Point(764, 178);
+            this.lblDest.Name = "lblDest";
+            this.lblDest.Size = new System.Drawing.Size(189, 29);
+            this.lblDest.TabIndex = 1;
+            this.lblDest.Text = "Waiting on Send";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(753, 72);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(128, 42);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(144, 83);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(566, 26);
+            this.txtFile.TabIndex = 3;
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(411, 122);
+            this.txtFileName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(300, 26);
+            this.txtFileName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(279, 120);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "FIlename";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(600, 169);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(112, 48);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lblConnection
+            // 
+            this.lblConnection.AutoSize = true;
+            this.lblConnection.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConnection.Location = new System.Drawing.Point(40, 539);
+            this.lblConnection.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblConnection.Name = "lblConnection";
+            this.lblConnection.Size = new System.Drawing.Size(204, 33);
+            this.lblConnection.TabIndex = 7;
+            this.lblConnection.Text = "Disconnected";
+            this.lblConnection.Click += new System.EventHandler(this.lblConnection_Click);
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(529, 334);
+            this.picBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(502, 394);
+            this.picBox.TabIndex = 8;
+            this.picBox.TabStop = false;
+            this.picBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 306);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Display Photo";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtPhoto
+            // 
+            this.txtPhoto.Location = new System.Drawing.Point(31, 352);
+            this.txtPhoto.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPhoto.Name = "txtPhoto";
+            this.txtPhoto.Size = new System.Drawing.Size(439, 26);
+            this.txtPhoto.TabIndex = 10;
+            // 
+            // btnPhoto
+            // 
+            this.btnPhoto.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhoto.Location = new System.Drawing.Point(176, 398);
+            this.btnPhoto.Name = "btnPhoto";
+            this.btnPhoto.Size = new System.Drawing.Size(128, 42);
+            this.btnPhoto.TabIndex = 11;
+            this.btnPhoto.Text = "Browse";
+            this.btnPhoto.UseVisualStyleBackColor = true;
+            this.btnPhoto.Click += new System.EventHandler(this.btnPhoto_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1163, 892);
+            this.Controls.Add(this.btnPhoto);
+            this.Controls.Add(this.txtPhoto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.lblConnection);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblDest);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDest;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblConnection;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPhoto;
+        private System.Windows.Forms.Button btnPhoto;
+    }
+}
+
---
+++ FTP_Server/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FTP_Server
+{
+    public partial class Form1 : Form
+    {
+        Int32 port1 = 5050;
+        string filepath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {           
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFile.Text = folderBrowserDialog1.SelectedPath;                
+            }
+        }
+        private void ReceiveFile(String FilePath)
+        {
+            lblDest.Text = "Uploading File......";
+
+            //Activate server and listen on port for an IP
+            TcpListener listener = new TcpListener(IPAddress.Any, port1);
+            listener.Start();
+            Socket client = listener.AcceptSocket();
+                
+            //After connection open file to be written, allocate A BUFFER 64Kb, Receive the data
+            //and write to the file
+            if(client.Connected) lblConnection.Text = "Connected";
+            Stream fileStream = File.OpenWrite(FilePath);
+            int chunkSize = 64 *1024;
+            int dataRead = 0;
+            Byte[] recBuffer = new Byte[chunkSize];
+            while ((dataRead = client.Receive(recBuffer, recBuffer.Length, SocketFlags.None)) > 0)
+            {
+                byte[] actual = new byte[dataRead];
+                Buffer.BlockCopy(recBuffer, 0, actual, 0, dataRead);
+                fileStream.Write(actual, 0, actual.Length);
+            }
+            fileStream.Close();
+            if (!client.Connected) lblConnection.Text = "Disconnected";
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if ((txtFileName.Text == "") || (txtFile.Text == "")) {
+                MessageBox.Show("Enter Filepath and Filename!");
+            }
+            else
+            {
+                filepath = txtFile.Text + "\\" + txtFileName.Text;
+                ReceiveFile(filepath);
+                lblDest.Text = "File Received......";
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPhoto_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtPhoto.Text = openFileDialog1.FileName;
+                picBox.Image = new Bitmap(openFileDialog1.FileName);
+                picBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            }
+        }
+
+        private void lblConnection_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
